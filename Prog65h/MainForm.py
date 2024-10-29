6a052366414e71cf092f93628b5535e9635d9402
DeadDuc <wambura.c@stu.janesville.k12.wi.us>
2024-10-29 09:39:11 -0500

    i finished 65h fast

---
+++ Prog65h/MainForm.py
@@ -0,0 +1,198 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self._label1 = System.Windows.Forms.Label()
+		self._label2 = System.Windows.Forms.Label()
+		self._label3 = System.Windows.Forms.Label()
+		self._label4 = System.Windows.Forms.Label()
+		self._label5 = System.Windows.Forms.Label()
+		self._textBox1 = System.Windows.Forms.TextBox()
+		self._textBox2 = System.Windows.Forms.TextBox()
+		self._textBox3 = System.Windows.Forms.TextBox()
+		self._label6 = System.Windows.Forms.Label()
+		self.SuspendLayout()
+		# 
+		# button1
+		# 
+		self._button1.BackColor = System.Drawing.Color.PaleTurquoise
+		self._button1.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.Location = System.Drawing.Point(12, 327)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(147, 59)
+		self._button1.TabIndex = 0
+		self._button1.Text = "Convert"
+		self._button1.UseVisualStyleBackColor = False
+		self._button1.Click += self.Button1Click
+		# 
+		# button2
+		# 
+		self._button2.BackColor = System.Drawing.Color.PaleTurquoise
+		self._button2.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.Location = System.Drawing.Point(165, 327)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(147, 59)
+		self._button2.TabIndex = 1
+		self._button2.Text = "Clear"
+		self._button2.UseVisualStyleBackColor = False
+		self._button2.Click += self.Button2Click
+		# 
+		# button3
+		# 
+		self._button3.BackColor = System.Drawing.Color.PaleTurquoise
+		self._button3.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.Location = System.Drawing.Point(318, 327)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(147, 59)
+		self._button3.TabIndex = 2
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = False
+		self._button3.Click += self.Button3Click
+		# 
+		# label1
+		# 
+		self._label1.BackColor = System.Drawing.Color.MediumTurquoise
+		self._label1.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label1.Location = System.Drawing.Point(13, 13)
+		self._label1.Name = "label1"
+		self._label1.Size = System.Drawing.Size(452, 60)
+		self._label1.TabIndex = 3
+		self._label1.Text = "British Conversion"
+		self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# label2
+		# 
+		self._label2.BackColor = System.Drawing.Color.Cyan
+		self._label2.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label2.Location = System.Drawing.Point(12, 79)
+		self._label2.Name = "label2"
+		self._label2.Size = System.Drawing.Size(146, 49)
+		self._label2.TabIndex = 4
+		self._label2.Text = "Pounds"
+		self._label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# label3
+		# 
+		self._label3.BackColor = System.Drawing.Color.Cyan
+		self._label3.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label3.Location = System.Drawing.Point(12, 128)
+		self._label3.Name = "label3"
+		self._label3.Size = System.Drawing.Size(146, 49)
+		self._label3.TabIndex = 5
+		self._label3.Text = "Shillings"
+		self._label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# label4
+		# 
+		self._label4.BackColor = System.Drawing.Color.Cyan
+		self._label4.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label4.Location = System.Drawing.Point(12, 177)
+		self._label4.Name = "label4"
+		self._label4.Size = System.Drawing.Size(146, 49)
+		self._label4.TabIndex = 6
+		self._label4.Text = "Pence"
+		self._label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# label5
+		# 
+		self._label5.BackColor = System.Drawing.Color.PowderBlue
+		self._label5.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label5.Location = System.Drawing.Point(13, 235)
+		self._label5.Name = "label5"
+		self._label5.Size = System.Drawing.Size(146, 78)
+		self._label5.TabIndex = 7
+		self._label5.Text = "Modern Notation"
+		self._label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# textBox1
+		# 
+		self._textBox1.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._textBox1.Location = System.Drawing.Point(165, 81)
+		self._textBox1.Name = "textBox1"
+		self._textBox1.Size = System.Drawing.Size(299, 46)
+		self._textBox1.TabIndex = 8
+		# 
+		# textBox2
+		# 
+		self._textBox2.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._textBox2.Location = System.Drawing.Point(165, 130)
+		self._textBox2.Name = "textBox2"
+		self._textBox2.Size = System.Drawing.Size(299, 46)
+		self._textBox2.TabIndex = 9
+		# 
+		# textBox3
+		# 
+		self._textBox3.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._textBox3.Location = System.Drawing.Point(165, 179)
+		self._textBox3.Name = "textBox3"
+		self._textBox3.Size = System.Drawing.Size(299, 46)
+		self._textBox3.TabIndex = 10
+		# 
+		# label6
+		# 
+		self._label6.BackColor = System.Drawing.Color.PaleTurquoise
+		self._label6.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+		self._label6.Location = System.Drawing.Point(166, 235)
+		self._label6.Name = "label6"
+		self._label6.Size = System.Drawing.Size(298, 78)
+		self._label6.TabIndex = 11
+		self._label6.Text = "label6"
+		self._label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# MainForm
+		# 
+		self.BackColor = System.Drawing.Color.DarkCyan
+		self.ClientSize = System.Drawing.Size(481, 398)
+		self.Controls.Add(self._label6)
+		self.Controls.Add(self._textBox3)
+		self.Controls.Add(self._textBox2)
+		self.Controls.Add(self._textBox1)
+		self.Controls.Add(self._label5)
+		self.Controls.Add(self._label4)
+		self.Controls.Add(self._label3)
+		self.Controls.Add(self._label2)
+		self.Controls.Add(self._label1)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
+		self.Name = "MainForm"
+		self.Text = "Prog65h"
+		self.ResumeLayout(False)
+		self.PerformLayout()
+
+
+	def Button1Click(self, sender, e):
+		# Collect data from textboxes and create variables
+		lbs = float(self._textBox1.Text)
+		shil = float(self._textBox2.Text)
+		pence = float(self._textBox3.Text)
+		# Convert shillings and old pence to new pence
+		newpence = (float(shil) * 5) * .01
+		oldpence = (float(pence) * 8.33333333333) * .0001
+		# Adding and then printing
+		prenewnotation = float(lbs) + float(newpence) + float(oldpence)
+		newnotation = round(prenewnotation, 2)
+		self._label6.Text = str(newnotation)
+		pass
+
+	def Button2Click(self, sender, e):
+		self._label6.Text = ""
+		self._textBox1.Text = ""
+		self._textBox2.Text = ""
+		self._textBox3.Text = ""
+		pass
+
+	def Button3Click(self, sender, e):
+		Application.Exit()
+		pass
