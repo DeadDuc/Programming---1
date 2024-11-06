ed6d909a8f2a65d1d6a43d46a5c6585bcfb2ccd8
DeadDuc <wambura.c@stu.janesville.k12.wi.us>
2024-11-06 09:32:17 -0600

    push

---
+++ StringStuff/Program.py
@@ -0,0 +1,22 @@
+﻿thing = "HELLO world HERE is SOME text"
+# Only print lowercase function
+print(thing.lower())
+# Only print uppercase function
+print(thing.upper())
+# Capitalize first letter function
+print(thing.capitalize())
+# Capitalize everything function
+print(thing.title())
+# Check if the entire string has digits function
+print(thing.isdigit())
+# See index and rindex function
+print(thing.find("world"))
+print(thing.count("E"))
+# Reverse a string function (palindrome)
+print(thing[::-1])
+# Split up strings function
+print(thing.split(" "))
+# Print string multiple times
+print("Hello" * 5)
+
+input()
