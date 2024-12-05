060e6a6d3c34f3944dadfcbf5dd0a31c8b277611
DeadDuc <wambura.c@stu.janesville.k12.wi.us>
2024-12-05 09:02:52 -0600

    commit and push

--- Prog152a/MainForm.py
+++ Prog152a/MainForm.py
@@ -9,61 +9,89 @@ class MainForm(Form):
 		self.InitializeComponent()
 	
 	def InitializeComponent(self):
-		self._label1 = System.Windows.Forms.Label()
 		self._button1 = System.Windows.Forms.Button()
 		self._button2 = System.Windows.Forms.Button()
 		self._button3 = System.Windows.Forms.Button()
+		self._listBox1 = System.Windows.Forms.ListBox()
 		self.SuspendLayout()
-		# 
-		# label1
-		# 
-		self._label1.BackColor = System.Drawing.Color.LightGray
-		self._label1.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._label1.Location = System.Drawing.Point(13, 13)
-		self._label1.Name = "label1"
-		self._label1.Size = System.Drawing.Size(609, 360)
-		self._label1.TabIndex = 0
-		self._label1.Text = "label1"
 		# 
 		# button1
 		# 
 		self._button1.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._button1.Location = System.Drawing.Point(13, 376)
+		self._button1.Location = System.Drawing.Point(12, 364)
 		self._button1.Name = "button1"
-		self._button1.Size = System.Drawing.Size(205, 86)
+		self._button1.Size = System.Drawing.Size(205, 112)
 		self._button1.TabIndex = 1
 		self._button1.Text = "Calculate"
 		self._button1.UseVisualStyleBackColor = True
+		self._button1.Click += self.Button1Click
 		# 
 		# button2
 		# 
 		self._button2.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._button2.Location = System.Drawing.Point(224, 376)
+		self._button2.Location = System.Drawing.Point(224, 364)
 		self._button2.Name = "button2"
-		self._button2.Size = System.Drawing.Size(196, 86)
+		self._button2.Size = System.Drawing.Size(196, 112)
 		self._button2.TabIndex = 2
 		self._button2.Text = "Clear"
 		self._button2.UseVisualStyleBackColor = True
+		self._button2.Click += self.Button2Click
 		# 
 		# button3
 		# 
 		self._button3.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._button3.Location = System.Drawing.Point(426, 376)
+		self._button3.Location = System.Drawing.Point(426, 364)
 		self._button3.Name = "button3"
-		self._button3.Size = System.Drawing.Size(196, 86)
+		self._button3.Size = System.Drawing.Size(196, 112)
 		self._button3.TabIndex = 3
 		self._button3.Text = "Exit"
 		self._button3.UseVisualStyleBackColor = True
+		self._button3.Click += self.Button3Click
+		# 
+		# listBox1
+		# 
+		self._listBox1.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._listBox1.FormattingEnabled = True
+		self._listBox1.ItemHeight = 38
+		self._listBox1.Location = System.Drawing.Point(12, 12)
+		self._listBox1.Name = "listBox1"
+		self._listBox1.Size = System.Drawing.Size(609, 346)
+		self._listBox1.TabIndex = 4
 		# 
 		# MainForm
 		# 
 		self.BackColor = System.Drawing.SystemColors.ActiveBorder
-		self.ClientSize = System.Drawing.Size(633, 474)
+		self.ClientSize = System.Drawing.Size(633, 488)
+		self.Controls.Add(self._listBox1)
 		self.Controls.Add(self._button3)
 		self.Controls.Add(self._button2)
 		self.Controls.Add(self._button1)
-		self.Controls.Add(self._label1)
 		self.Name = "MainForm"
 		self.Text = "Prog152a"
 		self.ResumeLayout(False)
 
+
+	def Button3Click(self, sender, e):
+		Application.Exit()
+		pass
+
+	def Button2Click(self, sender, e):
+		self._listBox1.Items.Clear()
+		pass
+
+	def Button1Click(self, sender, e):
+#		3223
+		finalsum = 15586428
+		heading = "Multiple of Three\tSum"
+		self._listBox1.Items.Add(heading)
+		sum = 0
+		num = 0
+		while(sum < finalsum):
+			for num in range(1, 3223+1):
+				sum = 3 + (num * 3)
+				stringnum = num * 3
+				line = str(stringnum) + "\t\t" + str(sum)
+				self._listBox1.Items.Add(line)
+				if sum == finalsum:
+					break
+		pass
--- Prog152b/MainForm.py
+++ Prog152b/MainForm.py
@@ -12,7 +12,9 @@ class MainForm(Form):
 		self._label1 = System.Windows.Forms.Label()
 		self._textBox1 = System.Windows.Forms.TextBox()
 		self._button1 = System.Windows.Forms.Button()
-		self._label2 = System.Windows.Forms.Label()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self._listBox1 = System.Windows.Forms.ListBox()
 		self.SuspendLayout()
 		# 
 		# label1
@@ -36,29 +38,58 @@ class MainForm(Form):
 		# 
 		# button1
 		# 
-		self._button1.Location = System.Drawing.Point(92, 283)
+		self._button1.BackColor = System.Drawing.Color.SandyBrown
+		self._button1.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.Location = System.Drawing.Point(12, 295)
 		self._button1.Name = "button1"
-		self._button1.Size = System.Drawing.Size(75, 23)
+		self._button1.Size = System.Drawing.Size(263, 62)
 		self._button1.TabIndex = 2
-		self._button1.Text = "button1"
-		self._button1.UseVisualStyleBackColor = True
+		self._button1.Text = "Calculate"
+		self._button1.UseVisualStyleBackColor = False
 		self._button1.Click += self.Button1Click
 		# 
-		# label2
+		# button2
 		# 
-		self._label2.BackColor = System.Drawing.Color.Chocolate
-		self._label2.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._label2.Location = System.Drawing.Point(12, 76)
-		self._label2.Name = "label2"
-		self._label2.Size = System.Drawing.Size(673, 190)
-		self._label2.TabIndex = 3
-		self._label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+		self._button2.BackColor = System.Drawing.Color.SandyBrown
+		self._button2.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.Location = System.Drawing.Point(281, 295)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(212, 62)
+		self._button2.TabIndex = 4
+		self._button2.Text = "Clear"
+		self._button2.UseVisualStyleBackColor = False
+		self._button2.Click += self.Button2Click
+		# 
+		# button3
+		# 
+		self._button3.BackColor = System.Drawing.Color.SandyBrown
+		self._button3.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.Location = System.Drawing.Point(499, 295)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(186, 62)
+		self._button3.TabIndex = 5
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = False
+		self._button3.Click += self.Button3Click
+		# 
+		# listBox1
+		# 
+		self._listBox1.BackColor = System.Drawing.Color.Chocolate
+		self._listBox1.Font = System.Drawing.Font("Microsoft YaHei", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._listBox1.FormattingEnabled = True
+		self._listBox1.ItemHeight = 38
+		self._listBox1.Location = System.Drawing.Point(12, 57)
+		self._listBox1.Name = "listBox1"
+		self._listBox1.Size = System.Drawing.Size(673, 232)
+		self._listBox1.TabIndex = 6
 		# 
 		# MainForm
 		# 
 		self.BackColor = System.Drawing.Color.SaddleBrown
-		self.ClientSize = System.Drawing.Size(697, 344)
-		self.Controls.Add(self._label2)
+		self.ClientSize = System.Drawing.Size(697, 371)
+		self.Controls.Add(self._listBox1)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
 		self.Controls.Add(self._button1)
 		self.Controls.Add(self._textBox1)
 		self.Controls.Add(self._label1)
@@ -69,12 +100,24 @@ class MainForm(Form):
 
 
 	def Button1Click(self, sender, e):
+		heading = "Even Integer\tSum"
+		self._listBox1.Items.Add(heading)
 		variable = float(self._textBox1.Text)
 		sum = 0
 		number = 0
 		while(sum < variable):
 			number += 2
 			sum += number
-			self._label2.Text = str(number)
+			line = str(number) + "\t\t" + str(sum)
+			self._listBox1.Items.Add(line)
 			if sum == variable or sum >= variable:
 				break
+
+	def Button2Click(self, sender, e):
+		self._textBox1.Text = ""
+		self._listBox1.Items.Clear()
+		pass
+
+	def Button3Click(self, sender, e):
+		Application.Exit()
+		pass
